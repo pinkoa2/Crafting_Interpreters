7139547167336f5bc74675cea9d1e67955dafe25
Alex Pinkowski <apinkow27@gmail.com>
2023-09-22 15:41:38 -0400

    feat: finished basic command line interface to get started

--- code/lox/src/main.rs
+++ code/lox/src/main.rs
@@ -1,12 +1,16 @@
-struct Lox {}
-
-impl Lox {
-    fn test(&self) {
-        println!("Test");
-    }
-}
+mod lox;
+use lox::Lox;
+use std::env;
 
 fn main() {
-    let lox = Lox {};
-    lox.test()
+    // The first args[0] is going to be /target/*/lox
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() == 1 {
+        Lox::run_prompt()
+    } else if args.len() == 2 {
+        Lox::run_file(args.get(1).unwrap().clone());
+    } else {
+        println!("More than 1 arg was given, can't proceed, exitting...");
+    }
 }
