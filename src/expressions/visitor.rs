068274ad492b0c300ba113bb586103fa0d5e70b6
Alex Pinkowski <apinkow27@gmail.com>
2023-11-04 15:58:26 -0400

    docs: clean up

--- src/expressions/visitor.rs
+++ src/expressions/visitor.rs
@@ -6,23 +6,3 @@ pub trait Visitor<ReturnType> {
     fn visit_unary(&self, element: &Unary) -> ReturnType;
     fn visit_grouping(&self, element: &Grouping) -> ReturnType;
 }
-
-/*
- 
-Is Dr. Asnis taking new patients and would like to make an appointment for my back
-
-
-Do you take my insurance 
-
-    Blue Cross Blue Shield Anthem
-
-    866-211-6588
-
-
-    617-726-7500
-
-    8236044
-
-    1175 Cambridge Street Boston 
-    165kj
-*/
